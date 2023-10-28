87b98d24c177e4e20a46bbcc4de0b651417ca714
Helal Sheikh <106902696+helalsheikh@users.noreply.github.com>
2023-10-28 14:17:55 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".menu-icon");
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    menu.classList.toggle("open-menu");
+}
+
+
+      var swiper = new Swiper(".services-content", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 50000,
+          disableOnInteraction: false,
+        },
+        
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+  //  header
+     let header = document.querySelector("header");
+
+     window.addEventListener("scroll", ()  =>  {
+      header.classList.toggle("shadow", window.scrollY>0)
+     });
